3287f767a3acbab6e37e6581ca372e123493fe85
Osman <Osman@LAPTOP-D2M555L5>
2021-12-05 14:59:42 -0500

    Math Summary Extra Credit Stuff NonesenseAdd project files.

---
+++ Pages/ComingSoon.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathSummaries.Pages
+{
+    public class ComingSoonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Units/Unit3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathSummaries.Pages.Units
+{
+    public class Unit3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
