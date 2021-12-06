530081d4bed9ad49ad2f2448683d26a1b36b80da
Abdo Ahmed <Abdelrahman12345678910@outlook.com>
2021-12-05 23:40:40 -0500

    dark mode

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,49 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var checkbox = document.querySelector('#theme-toggle');
 
-// Write your JavaScript code.
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        document.documentElement.classList.add('dark-mode');
+        document.querySelectorAll('.un-inverted').forEach((result) => {
+            result.classList.add('uninvert');
+        });
+        window.localStorage.setItem('Dark Mode', 'On');
+    } else {
+        document.documentElement.classList.remove('dark-mode');
+        document.querySelectorAll('.un-inverted').forEach((result) => {
+            result.classList.remove('uninvert');
+        });
+        window.localStorage.setItem('Dark Mode', 'Off');
+    }
+});
+
+window.onload = function exampleFunction() {
+    const theme = localStorage.getItem('Dark Mode');
+    if (theme == 'On') {
+        checkbox.checked = true;
+        document.documentElement.classList.add('dark-mode');
+        document.querySelectorAll('.un-inverted').forEach((result) => {
+            result.classList.add('uninvert');
+        });
+    } else {
+        checkbox.checked = false;
+        document.documentElement.classList.remove('dark-mode');
+        document.querySelectorAll('.un-inverted').forEach((result) => {
+            result.classList.remove('uninvert');
+        });
+    }
+}
+
+
+
+
+
+
+//document.onkeypress = function (e) {
+//    e = e || window.event;
+//    if (e.keyCode === 13) {
+//        document.documentElement.classList.toggle('dark-mode');
+//        document.querySelectorAll('.un-inverted').forEach((result) => {
+//            result.classList.toggle('uninvert');
+//        });
+//    }
+//}
